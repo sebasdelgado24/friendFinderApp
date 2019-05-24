d7e9fa0905ba4f1f30886340de681a0d091b3dcc
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-05-24 18:00:55 -0500

    html routes survey and home done

--- FriendFinder/routing/htmlRoutes.js
+++ FriendFinder/routing/htmlRoutes.js
@@ -4,22 +4,18 @@ var path = require("path");
 
 //ROUTING
 module.exports = function(app) {
-//HTML GET Requests
-// The code below handles when the users visit a page.
-// On each of the cases below the user is shown an HTML page of content
+ //HTML GET Requests
+ // The code below handles when the users visit a page.
+ // On each of the cases below the user is shown an HTML page of content
 
-app.get("", function(req, res) {
-  res.sendFile(path.join(__dirname, ""));    
-});
+ app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));    
+ });
 
-app.get("", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));    
-});
-
-//If no matching route is found default to home
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, ""))
-});
+ //If no matching route is found default to home
+ app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));    
+ });
 };
 
 
